9fe17660319cb4cba342c1ce99516345d90fb6f7
tekihei2317 <tekihei2317@gmail.com>
2023-04-26 22:28:10 +0900

    create svelte app, tutorial 1~3

---
+++ src/routes/test/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import Nested from './Nested.svelte';
+	let numbers = [1, 2, 3, 4];
+	$: sum = numbers.reduce((sum, number) => sum + number, 0);
+
+	function addNumber() {
+		numbers = [...numbers, numbers.length + 1];
+	}
+</script>
+
+<button on:click={addNumber}>Add a number</button>
+
+<p>{numbers.join(' + ')} = {sum}</p>
+
+<Nested answer={sum} />
